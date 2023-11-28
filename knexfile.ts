aa183798dce13850ee55ecd455ff82b86c3fea23
Lucas Tavares <zlucas.np@gmail.com>
2023-11-28 20:25:28 -0300

    feat: configure knex.js with mysql

---
+++ knexfile.ts
@@ -0,0 +1,53 @@
+import 'dotenv/config';
+
+// Update with your config settings.
+
+const config = {
+  client: 'mysql2',
+  useNullAsDefault: true,
+  connection: {
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+  migrations: {
+    directory: `${__dirname}/src/database/migrations`,
+    extension: 'ts',
+  },
+
+  /*  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  }, */
+
+  /* production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  }, */
+};
+
+export default config;
