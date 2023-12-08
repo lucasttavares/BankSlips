5c9e7a9bbf05706aaa7a733cfb03a85f0a5cf082
Lucas Tavares <zlucas.np@gmail.com>
2023-12-08 18:19:55 -0300

    docs: add swagger documentation

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import 'dotenv/config';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocs from './swagger.json';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 require('./routes/bankslips.routes')(app);
 
