a77e35a95435f1fc86f0674eee5a1a7ed8d2e343
Lucas Tavares <zlucas.np@gmail.com>
2023-12-08 18:18:43 -0300

    feat: creates the method that lists the details of a slip and corrects the method that posts a slip

--- src/controllers/bankSlipsController.ts
+++ src/controllers/bankSlipsController.ts
@@ -1,43 +1,41 @@
 import { Request, Response } from 'express';
 import db from '../database/connection';
 import { v4 as uuidv4 } from 'uuid';
-import { generateToken } from '../utils/token-manipulator';
+import fineCaulculator from '../utils/functions/fineCaulculator';
 
 export const postSlip = async (request: Request, response: Response) => {
   const slip = request.body;
 
-  if (
-    slip.total_in_cents !== undefined &&
-    slip.due_date !== undefined &&
-    slip.customer !== undefined &&
-    typeof slip.total_in_cents === 'number' &&
-    typeof slip.due_date === 'string' &&
-    typeof slip.customer === 'string'
-  ) {
-    const uuid = uuidv4();
+  if (Object.keys(slip).length !== 0) {
+    if (
+      slip.total_in_cents !== null &&
+      slip.due_date !== null &&
+      slip.customer !== null &&
+      typeof slip.total_in_cents === 'number' &&
+      typeof slip.due_date === 'string' &&
+      typeof slip.customer === 'string'
+    ) {
+      const uuid = uuidv4();
 
-    try {
       await db('slips').insert({ ...slip, id: uuid });
-
       return response
-        .status(200)
+        .status(201)
         .send(
           await db('slips')
             .select('id', 'due_date', 'total_in_cents', 'customer', 'status')
             .where('id', uuid)
             .first(),
         );
-    } catch (err) {
-      console.log(err);
-      return response
-        .status(400)
-        .send('Bankslip not provided in the request body');
+    } else {
+      return response.status(422).send(
+        `<p>Invalid bankslip provided. The possible reasons are: <br/>
+        • A field of the provided bankslip was null or with invalid values</p>`,
+      );
     }
   } else {
-    return response.status(422).send(
-      `<p>Invalid bankslip provided. The possible reasons are: <br/>
-      • A field of the provided bankslip was null or with invalid values</p>`,
-    );
+    return response
+      .status(400)
+      .send('Bankslip not provided in the request body');
   }
 };
 
@@ -57,6 +55,25 @@ export const getSlips = async (request: Request, response: Response) => {
   }
 };
 
+export const getSlipsById = async (request: Request, response: Response) => {
+  const id = request.params.id;
+
+  try {
+    const slip = await db('slips').select('*').where({ id: id }).first();
+    const currentDate = new Date().getTime();
+    const dueDate = new Date(slip.due_date).getTime();
+
+    const fine = fineCaulculator.calculate(slip, currentDate, dueDate);
+
+    return response.status(200).send({ ...slip, fine: fine });
+  } catch (err) {
+    console.log(err);
+    return response
+      .status(404)
+      .send('Bankslip not found with the specified id');
+  }
+};
+
 export const paySlip = async (request: Request, response: Response) => {
   const id = request.params.id;
   const slip = request.body;
@@ -66,7 +83,8 @@ export const paySlip = async (request: Request, response: Response) => {
     .update({ status: 'PAID', payment_date: slip.payment_date });
 
   if (paydedSlip > 0) {
-    return response.status(200).send('No content');
+    console.log(paydedSlip);
+    return response.status(204).send();
   } else {
     return response
       .status(404)
