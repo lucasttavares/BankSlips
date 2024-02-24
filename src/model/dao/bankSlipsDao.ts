d7368a8a43ead933411e3a487da095e2b1cedaa3
Lucas Tavares <zlucas.np@gmail.com>
2024-02-24 16:00:43 -0300

    Revert "Revert "refactor: ajust bankSlips methods""
    This reverts commit 816e23eaa59068877a51f50f1e40ddc8ead69887.

---
+++ src/model/dao/bankSlipsDao.ts
@@ -0,0 +1,37 @@
+import db from '../connection/connection';
+
+export default class BankSlipsDao {
+  public id?: string;
+  public due_date: Date;
+  public total_in_cents: number;
+  public customer: string;
+  public status?: string;
+  public payment_date: Date;
+
+  public static async findAll() {
+    return await db('slips').select('*');
+  }
+
+  public static async add(slip: BankSlipsDao, id: string) {
+    return await db('slips').insert({ ...slip, id: id });
+  }
+
+  public static async findById(id: string) {
+    return await db('slips').select('*').where({ id: id }).first();
+  }
+
+  public static async findAndCancel(id: string, status: string) {
+    return await db('slips').where({ id: id }).update({ status: status });
+  }
+
+  public static async findAndPay(
+    id: string,
+    status: string,
+    payment_date: Date,
+  ) {
+    return await db('slips').where({ id: id }).update({
+      status: status,
+      payment_date: payment_date,
+    });
+  }
+}
