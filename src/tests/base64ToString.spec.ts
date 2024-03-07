a6e2f0d2b7817cc50ef4d6cf9cb4814f14e8db4a
Lucas Tavares <zlucas.np@gmail.com>
2024-03-07 15:30:27 -0300

    refactor: Base64ToString test

--- src/tests/base64ToString.spec.ts
+++ src/tests/base64ToString.spec.ts
@@ -1,9 +1,9 @@
 import { base64ToString } from '../utils/base64ToString';
 
-describe('base64ToString', () => {
-  test('should decode base64 string correctly', () => {
-    const base64String = 'VGhpcyBpcyBhIHRlc3QgYmFzZTY0IHN0cmluZw==';
-    const expectedOutput = 'This is a test base64 string';
-    expect(base64ToString(base64String)).toEqual(expectedOutput);
+describe('Base64ToString', () => {
+  test('Converter', () => {
+    const string = 'dGVzdA==';
+
+    expect(base64ToString(string)).toEqual('test');
   });
 });
