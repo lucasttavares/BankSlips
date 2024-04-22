0adf732869c57be253098ab51fa4473e5759cc3f
Lucas <zlucas.np@gmail.com>
2024-04-22 17:22:35 -0300

    refactor: alter class name

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,29 +1,37 @@
 import { Request, Response } from 'express';
 import HttpStatusCode from '../utils/enum/httpStatusCode';
 import AdminRepository from '../database/AdminRepository';
-import AdminServices from '../services/adminServices';
+import AdminServices from '../services/AdminServices';
 import TokenManipulator from '../utils/tokenManipulator';
 import { base64ToString } from '../utils/base64ToString';
 
 export default class AuthController {
-  public static async register(request: Request, response: Response) {
+  public repository: AdminRepository;
+  public services: AdminServices;
+
+  constructor() {
+    this.repository = new AdminRepository();
+    this.services = new AdminServices();
+  }
+
+  public async register(request: Request, response: Response) {
     const admin = request.body;
     try {
       return response
         .status(HttpStatusCode.OK)
-        .send(await AdminRepository.add(admin));
+        .send(await this.repository.add(admin));
     } catch (error) {
       return response.status(HttpStatusCode.BAD_REQUEST).send(error);
     }
   }
 
-  public static async singIn(request: Request, response: Response) {
+  public async singIn(request: Request, response: Response) {
     const admin = request.body;
     try {
       return response
         .status(HttpStatusCode.OK)
         .send(
-          await AdminServices.verifyAdminCredentials(
+          await this.services.verifyAdminCredentials(
             admin.email,
             admin.password,
           ),
@@ -33,7 +41,7 @@ export default class AuthController {
     }
   }
 
-  public static async refreshToken(request: Request, response: Response) {
+  public async refreshToken(request: Request, response: Response) {
     const { authorization } = request.headers;
     const admin = JSON.parse(base64ToString(authorization!.split('.')[1]));
 
